31d5045c1a3913ed0283eaf93272513b315d610a
achraf <achrafsikal@gmail.com>
2025-03-10 14:44:58 +0000

    Feat: Create UserController with sanctum (apitokens) and error handling, with validation and routes

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,13 @@
 <?php
 
-use App\Http\Controllers\CarController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CarController;
+use App\Http\Controllers\UserController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 Route::apiResource('cars',CarController::class);
+Route::post('register', [UserController::class, 'createUser']);
+Route::post('login', [UserController::class, 'loginUser']);
