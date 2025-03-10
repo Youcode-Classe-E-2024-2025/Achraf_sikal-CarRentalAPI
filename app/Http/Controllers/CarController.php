f3c84897dfc1cd798fa29d41499f0c78001b7205
achraf <achrafsikal@gmail.com>
2025-03-10 22:46:34 +0000

    Feat: Add manual validation by creating a Validator instance in store and show methods

--- app/Http/Controllers/CarController.php
+++ app/Http/Controllers/CarController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Models\Cars;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
 
 class CarController extends Controller
 {
@@ -17,31 +19,64 @@ class CarController extends Controller
 
     public function store(Request $request)
     {
-        $validatedData = $request->validate([
-            'make' => 'required|string|max:255',
-            'model' => 'required|string|max:255',
-            'year' => 'required|integer|digits:4|before_or_equal:' . date('Y'),
-            'color' => 'required|string|max:50',
-            'registration_number' => 'required|string|unique:cars,registration_number|max:255',
-            'price_per_day' => 'required|numeric|min:0.01',
-            'available' => 'nullable|boolean',
-        ]);
+        try {
+            $validatedCar = Validator::make(
+                $request->all(),
+                [
+                'make' => 'required|string|max:255',
+                'model' => 'required|string|max:255',
+                'year' => 'required|integer|digits:4|before_or_equal:' . date('Y'),
+                'color' => 'required|string|max:50',
+                'registration_number' => 'required|string|unique:cars,registration_number|max:255',
+                'price_per_day' => 'required|numeric|min:0.01',
+                'available' => 'nullable|boolean',
+            ]);
+            if ($validatedCar->fails()) {
+                return response()->json([
+                    'status' => false,
+                    'message' => 'validation error',
+                    'errors' => $validatedCar->errors()
+                ], 401);
+            }
 
-        $car = Cars::create([
-            'make' => $validatedData['make'],
-            'model' => $validatedData['model'],
-            'year' => $validatedData['year'],
-            'color' => $validatedData['color'],
-            'registration_number' => $validatedData['registration_number'],
-            'price_per_day' => $validatedData['price_per_day'],
-            'available' => $validatedData['available'] ?? true,
-        ]);
-        return $car;
+            $car = Cars::create([
+                'make' => $request->make,
+                'model' => $request->model,
+                'year' => $request->year,
+                'color' => $request->color,
+                'registration_number' => $request->registration_number,
+                'price_per_day' => $request->price_per_day,
+                'available' => $request->available ?? true,
+            ]);
+            return response()->json([
+                'status' => true,
+                'message' => 'Car Created Successfully',
+                'car' => $car
+            ], 200);
+
+        } catch (\Throwable $th) {
+            return response()->json([
+                'status' => false,
+                'message' => $th->getMessage()
+            ], 500);
+        }
     }
 
     public function show(Cars $Car)
     {
-        return $Car;
+        try {
+            return response()->json([
+                'status' => true,
+                'message' => 'Car Created Successfully',
+                'car' => $Car
+            ], 200);
+        } catch (\Throwable $th) {
+            return response()->json([
+                'status' => false,
+                'message' => 'An error occurred',
+                'error' => $th->getMessage()
+            ], 500);
+        }
     }
 
     public function update(Request $request, Cars $Car)
